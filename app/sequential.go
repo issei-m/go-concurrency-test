4398945a3ab832ecae52cce6f302556b2795dafa
Issei.M <issei.m7@gmail.com>
2022-02-27 21:11:36 +0900

    Implement some buffering system in `app.ProcessItems`

--- app/sequential.go
+++ app/sequential.go
@@ -6,15 +6,35 @@ import (
 	"os"
 )
 
+var bufferSize = 20
+
+// ProcessItems 引数の items を全件 item.ProcessItem で処理し、成功時も失敗時もログを出す.
+// 1個でも失敗した場合は処理を辞める.
+// また何らかのバッファリングしている想定で、20回に1回バッファをクリアする.
 func ProcessItems(items []item.Item) {
+	failed := false
+	processedCount := 0
+
 	for _, targetItem := range items {
-		if result, err := item.ProcessItem(targetItem); err != nil {
+		result, err := item.ProcessItem(targetItem)
+		if err != nil {
 			if _, err := fmt.Fprintln(os.Stderr, fmt.Sprintf("\033[31mProcessing failed: %s\033[0m", err.Error())); err != nil {
 				panic(err)
 			}
+			failed = true
 			break // 1個でも失敗したら処理を抜ける
-		} else {
-			fmt.Println(result)
 		}
+
+		fmt.Println(result)
+
+		processedCount++
+
+		if processedCount%bufferSize == 0 {
+			fmt.Println("Flush buffer!")
+		}
+	}
+
+	if !failed && processedCount%bufferSize > 0 {
+		fmt.Println("Flush buffer!")
 	}
 }
