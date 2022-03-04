8f896bd6f7ca3210558c88226e63b08bd944b9e9
Issei.M <issei.m7@gmail.com>
2022-03-04 10:18:10 +0900

    Fix bug/Enhance concurrent
    - We had broken the loop where we receive from chTaskResults when have detected error, but it could make deadlock when the task has finished (that could be) after that because nobody receives chTaskResults
    - We now break the loop where we push an item to chItems when have detected error to improve performance

--- app/concurrent.go
+++ app/concurrent.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"go_concurrency_test/item"
 	"go_concurrency_test/logger"
-	"runtime"
-	"sync"
+	"sync/atomic"
 )
 
 type taskResult struct {
@@ -15,7 +14,6 @@ type taskResult struct {
 }
 
 func ProcessItemsConcurrently(items []item.Item, concurrency int) {
-	numItems := len(items)
 	var chItems = make(chan item.Item)
 	var chTaskResults = make(chan *taskResult)
 
@@ -25,84 +23,75 @@ func ProcessItemsConcurrently(items []item.Item, concurrency int) {
 		cancel()
 	}()
 
-	var wg sync.WaitGroup
-
+	numWorkers := int32(concurrency)
 	for i := 0; i < concurrency; i++ {
-		wg.Add(1)
 		go func(workerID int) {
 			defer func() {
-				logger.Debug(fmt.Sprintf("[WORKER %d] Terminated, bye (remaining %d worker(s))", workerID, runtime.NumGoroutine()-2)) // これから削除される自分自身と、 main goroutine の分を除く.
-				wg.Done()
+				atomic.AddInt32(&numWorkers, -1)
+				logger.Debug(fmt.Sprintf("[WORKER %d] Terminated, bye (remaining %d worker(s))", workerID, numWorkers))
 			}()
 
-		LOOP:
 			for targetItem := range chItems {
 				select {
 				case <-ctx.Done():
 					logger.Debug(fmt.Sprintf("[WORKER %d] Received Item(%d) to process but cancellation has been requested, so discard it and terminate this worker", workerID, targetItem))
-					break LOOP
 				default:
-				}
-
-				logger.Debug(fmt.Sprintf("[WORKER %d] Receved Item(%d) to process", workerID, targetItem))
-				result, err := item.ProcessItem(targetItem)
-				taskResult := &taskResult{result: result, err: err}
-
-				select {
-				case <-ctx.Done():
-					logger.Debug(fmt.Sprintf("[WORKER %d] Processed Item(%d) but cancellation has been requested, so discard it", workerID, targetItem))
-				default:
-					logger.Debug(fmt.Sprintf("[WORKER %d] Processed Item(%d), pushing the result", workerID, targetItem))
-					chTaskResults <- taskResult
+					logger.Debug(fmt.Sprintf("[WORKER %d] Receved Item(%d) to process", workerID, targetItem))
+					result, err := item.ProcessItem(targetItem)
+					chTaskResults <- &taskResult{result: result, err: err}
 				}
 			}
 		}(i + 1)
 	}
 
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-
-		processedCount := 0
-		failed := false
-
 	LOOP:
-		for processedCount < numItems {
+		for _, targetItem := range items {
 			select {
 			case <-ctx.Done():
-				logger.Debug("Has already detected an error, so terminate the tasks")
+				logger.Debug(fmt.Sprintf("Cancellation has already been requested, so no more item will be pushed"))
 				break LOOP
-			case result := <-chTaskResults:
-				if result.err != nil {
-					cancel()
-					logger.Error(fmt.Sprintf("Processing failed: %s", result.err.Error()))
-					failed = true
-					break LOOP // 1個でも失敗したら処理を抜ける
-				}
-
-				logger.Info(result.result)
-
-				processedCount++
-
-				if processedCount%bufferSize == 0 {
-					logger.Info("Flush buffer!")
-				}
+			default:
+				chItems <- targetItem
 			}
 		}
-
-		if !failed && processedCount%bufferSize > 0 {
-			logger.Info("Flush buffer!")
-		}
+		close(chItems)
 	}()
 
-	for _, targetItem := range items {
+	processedCount := 0
+	failed := false
+
+	for numWorkers > 0 {
 		select {
-		case <-ctx.Done():
+		case result := <-chTaskResults:
+			if failed {
+				break
+			}
+
+			if result.err != nil {
+				// 1件でもエラーが起きたら以降の処理をキャンセルする.
+				// worker が並行に動いている為、タイミングによってキャンセル後もいくつかの処理は実行されうるので、その時点でこの goroutine を閉じてしまうと deadlock になるので注意.
+				// 従って、 chTaskResults の受信は worker が全ていなくなるまで続ける.
+
+				cancel()
+				logger.Error(fmt.Sprintf("Processing failed: %s", result.err.Error()))
+				failed = true
+				break
+			}
+
+			logger.Info(result.result)
+
+			processedCount++
+
+			if processedCount%bufferSize == 0 {
+				logger.Info("Flush buffer!")
+			}
 		default:
-			chItems <- targetItem
+			// `default` が無いと、タイミングによっては worker が全て終了した後に chTaskResults から受信を試みる (=deadlock) 事になるので注意
 		}
 	}
-	close(chItems)
 
-	wg.Wait()
+	if !failed && processedCount%bufferSize > 0 {
+		logger.Info("Flush buffer!")
+	}
 }
